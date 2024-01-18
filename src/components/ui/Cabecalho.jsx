312f0dd8a395ea77e46648d6b1fb338d85223524
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2024-01-18 10:17:59 -0300

    Exercicio Resolvido

--- src/components/ui/Cabecalho.jsx
+++ src/components/ui/Cabecalho.jsx
@@ -37,7 +37,37 @@ const StyledHeader = styled.header`
     font-size: 1.5rem;
     display: flex;
     justify-content: center;
-    justify-content: center;
     align-items: center;
+  }
+
+  img {
+    margin-right: 0.5rem;
+  }
+
+  div {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-direction: column;
+    padding-bottom: 1rem;
+  }
+
+  h1 a:hover,
+  h1 a:focus {
+    color: var(--cor-secundaria-hover);
+  }
+
+  /* Mini-exercicio
+  Crie aqui uma media query para telas a partir de 700px e faça as seguintes mudanças:
+Na div do cabeçalho, exibir o conteúdo lado a lado (lembrando que ali você tem o flex já sendo usado)
+Tamanho da fonte do PetShop aumentar para 2rem*/
+
+  @media screen and (min-width: 700px) {
+    div {
+      flex-direction: row;
+    }
+    h1 a {
+      font-size: 2rem;
+    }
   }
 `;
