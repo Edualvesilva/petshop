5e2a4ebcf89c35c10ff1eef0495748814c0a1915
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2024-01-18 08:33:52 -0300

    Colocando Link no H1

--- src/components/ui/Cabecalho.jsx
+++ src/components/ui/Cabecalho.jsx
@@ -1,9 +1,12 @@
 import Menu from "./Menu";
+import Link from "next/link";
 export default function Cabecalho() {
   return (
     <>
       <header>
-        <h1>PetShop</h1>
+        <h1>
+          <Link href="/">PetShop</Link>
+        </h1>
         <Menu />
       </header>
     </>
