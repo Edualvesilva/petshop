9111eb2aa0142d3973b0d48e72f5417233814211
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2024-01-18 08:45:35 -0300

    Adição de imagem ao cabeçalho

--- src/components/ui/Cabecalho.jsx
+++ src/components/ui/Cabecalho.jsx
@@ -1,10 +1,17 @@
 import Menu from "./Menu";
 import Link from "next/link";
+import Image from "next/image";
 export default function Cabecalho() {
   return (
     <>
       <header>
         <h1>
+          <Image
+            src="/images/logo.png"
+            width={48}
+            height={48}
+            alt="Patinha dentro de um coração"
+          />
           <Link href="/">PetShop</Link>
         </h1>
         <Menu />
