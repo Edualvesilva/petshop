535ae083f81937dec15fbccba2066e2e12ee6d43
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2024-01-19 09:44:53 -0300

    Update contato.jsx

--- src/pages/contato.jsx
+++ src/pages/contato.jsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import styled from "styled-components";
+import Container from "@/components/ui/Container";
 export default function Contato() {
   return (
     <>
@@ -10,6 +11,33 @@ export default function Contato() {
       </Head>
       <StyledContato>
         <h2>Fale Conosco</h2>
+        <Container>
+          <form action="" method="post">
+            <div>
+              <label htmlFor="nome">Nome: </label>
+              <input type="text" name="nome" id="nome" />
+            </div>
+
+            <div>
+              <label htmlFor="email">Email: </label>
+              <input type="email" name="email" id="email" />
+            </div>
+
+            <div>
+              <label htmlFor="mensagem">Mensagem: </label>
+              <textarea
+                maxLength={500}
+                name="mensagem"
+                id="mensagem"
+                cols={30}
+                rows={8}
+              ></textarea>
+            </div>
+            <div>
+              <button type="submit">Enviar Mensagem</button>
+            </div>
+          </form>
+        </Container>
       </StyledContato>
     </>
   );
@@ -18,5 +46,44 @@ export default function Contato() {
 const StyledContato = styled.section`
   h2::before {
     content: "💌";
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+    max-width: 300px;
+    margin: 0 auto;
+  }
+
+  form div {
+    margin-bottom: 16px;
+  }
+
+  label {
+    display: block;
+    margin-bottom: 8px;
+    font-weight: bold;
+  }
+
+  input,
+  textarea {
+    width: 100%;
+    padding: 8px;
+    margin-bottom: 16px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+  }
+
+  button {
+    background-color: #4caf50;
+    color: #ffffff;
+    padding: 10px 15px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+  }
+
+  button:hover {
+    background-color: #45a049;
   }
 `;
