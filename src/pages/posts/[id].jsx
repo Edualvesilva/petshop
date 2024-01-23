29249764bf50947509f9550c47db81acfee1dd54
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2024-01-23 11:32:16 -0300

    Uso de path e fetch

--- src/pages/posts/[id].jsx
+++ src/pages/posts/[id].jsx
@@ -1,10 +1,28 @@
 import Container from "@/components/ui/Container";
 import styled from "styled-components";
 import Head from "next/head";
+import serverApi from "../api/server";
 
 export async function getStaticProps({ params }) {
   const { id } = params;
   console.log(id);
+
+  try {
+    const resposta = await fetch(`${serverApi}/posts/${id}`);
+    if (!resposta.ok) {
+      throw new Error(`Erro: ${resposta.staus} - ${resposta.statusText}`);
+    }
+
+    const dados = await resposta.json();
+
+    return {
+      props: {
+        post: dados,
+      },
+    };
+  } catch (error) {
+    console.error("Deu ruim: " + error.message);
+  }
 }
 
 export async function getStaticPaths() {
