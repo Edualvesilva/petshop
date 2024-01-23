e6fda67151a9f06b9d18055007a8c0ede40d0bca
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2024-01-23 11:24:13 -0300

    Adição da getStaticPaths

--- src/pages/posts/[id].jsx
+++ src/pages/posts/[id].jsx
@@ -1,6 +1,19 @@
 import Container from "@/components/ui/Container";
 import styled from "styled-components";
 import Head from "next/head";
+
+export async function getStaticProps({ params }) {
+  const { id } = params;
+  console.log(id);
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [],
+    fallback: "blocking",
+  };
+}
+
 export default function Post() {
   return (
     <>
