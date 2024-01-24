594af0272621ff9a035c1578465284e7220035fe
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2024-01-24 09:09:31 -0300

    comentarios das Funções

--- src/pages/posts/[id].jsx
+++ src/pages/posts/[id].jsx
@@ -4,6 +4,8 @@ import Head from "next/head";
 import serverApi from "../api/server";
 
 export async function getStaticProps({ params }) {
+  /* Utilizamos a prop params do getStaticProps para poder ter acesso aos parâmetros dinâmicos da rota condigurada nos links da Lista de Posts. Usamos a desestruturação para obter de forma direta o parâmetro chamado "id".  */
+
   const { id } = params;
   console.log(id);
 
@@ -27,9 +29,13 @@ export async function getStaticProps({ params }) {
   }
 }
 
+/* getStaticPaths é obrigatória quando se trata de trabalhar com páginas/rotas dinâmicas,ou seja, que dependem de parâmetros para serem construídos. */
 export async function getStaticPaths() {
   return {
+    /* paths fica vazio pois todos os caminhos devem ser gerados sob demanda, ou seja, no momento que a página for aberta.*/
     paths: [],
+
+    /* fallback fica como "blocking" para garantir que a página somente será renderizada após a conclusão da geração dos caminhos e dos dados estáticos */
     fallback: "blocking",
   };
 }
